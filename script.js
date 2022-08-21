8b5344a500e614f750798fbbcee85e75c6a04bc7
felixarjuna <felixarjuna.de@yahoo.com>
2022-08-21 21:46:34 +0200

    Looping over items with Javscript

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-const mobels = ["kallax", "meja-coklat", "Wooden shelf"];
+const mobels = [
+  {
+    name: "Kallax",
+    img: "./pictures/kallax.jpg",
+    price: 15,
+    info: "negotiable",
+  },
+  {
+    name: "Meja coklat",
+    img: "./pictures/meja-coklat.jpg",
+    price: 5,
+    info: "",
+  },
+  {
+    name: "Wooden shelf",
+    img: "./pictures/wooden-shelf.jpg",
+    price: 15,
+    info: "negotiable",
+  },
+];
+
+const container = document.querySelector("div.card-deck");
+
+mobels.map((mobel) => {
+  $("div.card-deck").append(`
+    <div class="card">
+        <img
+            class="card-img-top"
+            src="${mobel.img}"
+            alt="Card image cap"
+        />
+        <div class="card-body">
+        <h5 class="card-title">${mobel.name}</h5>
+        <p class="card-text">Price: ${mobel.price} €</p>
+        </div>
+        <p class="card-text">
+        <small class="text-muted">${mobel.info}</small>
+        </p>
+    </div>`);
+});
