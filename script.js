b830b416c02144d65aed701516db4f61ec527bb0
=benkrisna <benyaminitu@gmail.com>
2022-08-25 00:32:50 +0200

    Added hairdyer, tempat sampah, and setrika uap

--- script.js
+++ script.js
@@ -4,18 +4,42 @@ const mobels = [
     img: "./pictures/kallax.jpg",
     price: 15,
     info: "negotiable",
+    trivia: "Udah diambil Reggy",
   },
   {
     name: "Meja coklat",
     img: "./pictures/meja-coklat.jpg",
     price: 5,
-    info: "",
+    info: "negotiable, gratis pun gpp",
+    trivia: "Ambil plis guys, gratis pun gpp",
   },
   {
     name: "Wooden shelf",
     img: "./pictures/wooden-shelf.jpg",
-    price: 15,
+    price: 10,
     info: "negotiable",
+    trivia: "Baru banget woi padahal",
+  },
+  {
+    name: "Hair Dryer",
+    img: "./pictures/hair-dryer.jpg",
+    price: 5,
+    info: "negotiable",
+    trivia: "Ga bikin botak kok",
+  },
+  {
+    name: "Setrika uap",
+    img: "./pictures/setrika-uap.jpg",
+    price: 5,
+    info: "negotiable",
+    trivia: "Patrick pinjem ini buat Interview kerja, langsung dapet",
+  },
+  {
+    name: "Tong sampah",
+    img: "./pictures/tong-sampah.jpg",
+    price: 0,
+    info: "gratis woi",
+    trivia: "she's a keeper :)",
   },
 ];
 
@@ -33,6 +57,9 @@ mobels.map((mobel) => {
         </div>
         <p class="card-text">
         <small class="text-muted">${mobel.info}</small>
+        </p>
+        <p class="card-text">
+        <small class="text-muted">${mobel.trivia}</small>
         </p>
     </div>`);
 });
